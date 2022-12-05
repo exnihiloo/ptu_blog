9c3e9d44ce6a7a031f645688b7d21b942b7c558a
exnihiloo <deimante.jasaityte@gmail.com>
2022-12-05 19:17:35 +0200

    using java script set default author of blog

--- blog/forms.py
+++ blog/forms.py
@@ -16,7 +16,7 @@ class BlogPostForm(forms.ModelForm):
         widgets = {
             'title' : forms.TextInput(attrs = {'class':'form-control', 'placeholder': 'Title'}),
             'title_tag' : forms.TextInput(attrs = {'class':'form-control'}),
-            'author' : forms.Select(attrs = {'class':'form-control'}),
+            'author' : forms.TextInput(attrs = {'class':'form-control', 'value' : '', 'id':'author', 'type':'hidden'}),
             'topic' : forms.Select(choices = topics_list, attrs={'class':'form-control'}),
             'body' : forms.Textarea(attrs = {'class':'form-control', 'placeholder': 'Write your story here'}),
         }
