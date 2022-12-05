258e71c259a69a646ff68ca1c458177750c22a01
exnihiloo <deimante.jasaityte@gmail.com>
2022-12-05 11:17:04 +0200

    created list and detail views for blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('blog/<int:pk>', views.BlogDetail.as_view(), name='blogview'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import BlogPost
+# def home(request):
+#     return render(request, 'home.html', {})
 
-def home(request):
-    return render(request, 'home.html', {})
+
+class Home(ListView):
+    model = BlogPost
+    template_name = 'home.html'
+
+
+class BlogDetail(DetailView):
+    model = BlogPost
+    template_name = 'blog_details.html'
