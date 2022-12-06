784810ac97fe1d307a3ee3c3c3de6239051c7121
exnihiloo <deimante.jasaityte@gmail.com>
2022-12-06 11:13:34 +0200

    created Profile models and signals to user

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from PIL import Image
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, 
+        verbose_name=_("user"), 
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    photo = models.ImageField(_("photo"), upload_to='user_profile/photos', null=True, blank=True)
+    about = models.TextField(_("about"), max_length=2000)
+
+    def __str__(self) -> str:
+        return f"{self.user} profile"
+
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #     if self.photo:
+    #         photo = Image.open(self.photo.path)
+    #         if photo.width > 500 or photo.height > 500:
+    #             output_size = (500, 500)
+    #             photo.thumbnail(output_size)
+    #             photo.save(self.photo.path)
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordChangeView
 from django import forms
 from .forms import RegisterForm, UserUpdateForm, ChangePasswordForm
 from django.utils.translation import gettext_lazy as _
+from .models import Profile
 
 
 class UserRegistration(generic.CreateView):
@@ -27,4 +29,8 @@ class ChangePassword(PasswordChangeView):
     form_class = ChangePasswordForm
     success_url = reverse_lazy('home')
 
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
 
