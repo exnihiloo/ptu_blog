3a47bb4bd014b907d52f330ccddf8b42c21ee49e
exnihiloo <deimante.jasaityte@gmail.com>
2022-12-06 08:39:05 +0200

    ckeditor installation and application to website

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from datetime import datetime, date
+
 
 
 User = get_user_model()
@@ -23,7 +25,7 @@ class BlogPost(models.Model):
     title = models.CharField(_("title"), max_length = 255)
     title_tag = models.CharField(_("title tag"), max_length = 255, default='Fox blog')
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    body = models.TextField(_("body"))
+    body = RichTextField(_("body"), blank = True, null = True)
     creation_date = models.DateField(_("created"), auto_now_add=True)
     topic = models.CharField(_("topic"), max_length = 255, default='sport')
 
--- ptu_blog/settings.py
+++ ptu_blog/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog',
     'user_profile',
+    'ckeditor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
