d4e0d355c6b2afd056eba33e2871a9387ef71fc3
exnihiloo <deimante.jasaityte@gmail.com>
2022-12-08 14:47:40 +0200

    registration clean email and pwd, alerts on htmls

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -17,6 +17,19 @@ class RegisterForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 
+    def clean_email(self):
+            email = self.cleaned_data['email']
+            if User.objects.filter(email = email).exists():
+                raise forms.ValidationError(_('Please use another Email, that is already taken'))
+            return email
+    
+    def clean_password2(self):
+            cd = self.cleaned_data
+            if cd['password1'] != cd['password2']:
+                raise forms.ValidationError(_('Passwords do not match.'))
+            return cd['password2']
+
+
 
     def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
